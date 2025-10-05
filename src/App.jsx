b0ca2e56c03b993256a77c9913e5a82e46d0d98d
Apoorv-Saxena08 <coderapoorv08@gmail.com>
2025-10-06 01:36:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header"
+
+function Apoorv(){
+  return (
+    <div>
+      <Header></Header>
+    <h1>My name is Apoorv</h1>
+    </div>
+
+  )
+}
+
+export default Apoorv
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <div>
+            <h1>Header hu m</h1>
+        </div>
+    )
+}
+
+export default Header
