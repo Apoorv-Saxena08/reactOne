8e3e6ca963fc5f214088bcbd9a313c97e253adaa
Apoorv-Saxena08 <coderapoorv08@gmail.com>
2025-10-07 12:01:33 +0530

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import Header,{Profile} from "./Header"
 
 function Apoorv(){
-  function click(){
-    alert("You clicked me!!");
-  }
-   const name = "Apoorv";
-  return (
-    <div>
-      <Header></Header>
-    <h1>My name is {name}</h1>
-    <Profile></Profile>
-    <button onClick={click} >Click me</button>
-    </div>
+  const url = "https://imgs.search.brave.com/KlXQhUrtRYQm82JkljRcaWjJWIylJ0stCOhHJ4k0OHU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9pbWFnZS1jb29s/LWVhZ2xlLXdlYXJp/bmctZnVua3ktZ2xh/c3Nlc18xMTUxMTIz/LTY0OTc1LmpwZz9z/ZW10PWFpc19oeWJy/aWQmdz03NDAmcT04/MA"
 
+  function click(){
+    alert("Clicked!!!!!!");
+  }
+
+  return (
+    <>
+      <img src={url} alt="photo" className="photo" />
+
+      <ul>
+        <li>Invent a new car</li>
+        <li>By bye everyone</li>
+        <li>How are you??</li>
+      </ul>
+
+      <button onClick={click} >Click me</button>
+    </>
   )
 }
 
