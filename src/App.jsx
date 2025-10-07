57c83dd496e9c9c6104dd394ce59a4455e62f007
Apoorv-Saxena08 <coderapoorv08@gmail.com>
2025-10-07 12:38:28 +0530

    Made a counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
+import { useState } from "react"
 import Header,{Profile} from "./Header"
 
-function Apoorv(){
-  const url = "https://imgs.search.brave.com/KlXQhUrtRYQm82JkljRcaWjJWIylJ0stCOhHJ4k0OHU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9pbWFnZS1jb29s/LWVhZ2xlLXdlYXJp/bmctZnVua3ktZ2xh/c3Nlc18xMTUxMTIz/LTY0OTc1LmpwZz9z/ZW10PWFpc19oeWJy/aWQmdz03NDAmcT04/MA"
+// function click(){
+//   alert("Bhaar vala");
+// }
 
-  function click(){
-    alert("Clicked!!!!!!");
+function Apoorv(){
+   const [val, setVal] = useState(0);
+  const increase = () => {
+    setVal(val + 1);
   }
 
+  const decrease = () =>{
+    setVal(val -1);
+  }
   return (
     <>
-      <img src={url} alt="photo" className="photo" />
-
-      <ul>
-        <li>Invent a new car</li>
-        <li>By bye everyone</li>
-        <li>How are you??</li>
-      </ul>
-
-      <button onClick={click} >Click me</button>
+      <h1>Counter</h1>
+      <h2>Current value is {val}</h2>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
     </>
   )
 }
