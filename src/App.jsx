a0a52644efe368d1b6eedc098262f08bd1bea31f
Apoorv-Saxena08 <coderapoorv08@gmail.com>
2025-10-18 00:03:05 +0530

    Learning Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
 import { use, useState } from "react"
 import Header,{Profile} from "./Header"
+import User from "./User"
 
 // function click(){
 //   alert("Bhaar vala");
 // }
 
 function Apoorv(){
-  const [count,setCount] = useState(0);
-
-  const change =()=>{
-    setCount(count+1)
-  }
-
 
   return (
     <>
-      <h1>Multiple conditionals</h1>
-      <h2>{count}</h2>
-
-      <button onClick={change}>Change</button>
-
-      {
-        count==0?<p>Count is 0</p>
-        :count == 1?<p>Count is 1</p>
-        :count == 2?<p>Count is 2</p>
-        :<p>Count aapse se bhaar ho gya</p>
-      }
-
+    <h1>Props</h1>
+    <User name={"Virat kohli"} />
     </>
   )
 }
---
+++ src/User.jsx
@@ -0,0 +1,9 @@
+function User(props){
+    return(
+        <>
+        <h1>Hello {props.name}</h1>
+        </>
+    )
+}
+
+export default User;
