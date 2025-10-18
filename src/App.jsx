e7575616178d8dfe503cf586049b2edcae381110
Apoorv-Saxena08 <coderapoorv08@gmail.com>
2025-10-18 13:17:01 +0530

    Dynamic jsx pass with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { use, useState } from "react"
 import Header,{Profile} from "./Header"
 import User from "./User"
+import Wrapper from "./Wrapper"
 
 // function click(){
 //   alert("Bhaar vala");
 // }
 
 function Apoorv(){
-
+  const name = "Apoorv";
   return (
     <>
     <h1>Props</h1>
     <User name={"Virat kohli"} />
+    <Wrapper color="red" >
+      <h1>Hello Everyone</h1>
+      <h2>{name}</h2>
+    </Wrapper>
+
+    <Wrapper color="blue" >
+      <h1>Hello Everyone</h1>
+      <h2>Meer</h2>
+    </Wrapper>
     </>
   )
 }
