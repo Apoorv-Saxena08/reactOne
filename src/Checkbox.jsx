52fe68fa4a3e446eeb8f120841e5ecd94b195ead
Apoorv-Saxena08 <coderapoorv08@gmail.com>
2025-10-19 13:46:47 +0530

    Checkbox

---
+++ src/Checkbox.jsx
@@ -0,0 +1,12 @@
+function Checkboxes(){
+    return(
+        <div>
+            <h3>Select an option</h3>
+            <input type="checkbox" name="" id="php" />
+            <label htmlFor="php"></label>
+        </div>
+        
+    )
+}
+
+export default Checkboxes;
