fde670985db49529e03640b552e80a159a5dbd18
Apoorv-Saxena08 <coderapoorv08@gmail.com>
2025-10-19 13:34:49 +0530

    controlled comps

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,35 @@ import Wrapper from "./Wrapper";
 
 function App() {
   //const name = "Apoorv";
-  let [val,setVal] = useState("");
-
+  let [val, setVal] = useState("");
+  const [name,setName] = useState("");
+  const [pwd,setPwd] = useState("");
+  const [email,setEmail] = useState("");
+  
   return (
     <>
       <div>
-        <h1>Learning about input field</h1>
-        <input type="text" value={val} onChange={(event)=>setVal(event.target.value)} name="" id="" placeholder="Enter name" />
-        <h1>{val}</h1>
-        <button onClick={()=>setVal("")}>Clear</button>
+        <h1>Controlled components</h1>
+        <form action="" method="get">
+          <input type="text" name="" id="" placeholder="Enter name" value={name} onChange={(event)=>setName(event.target.value)} />
+          <br />
+          <br />
+          <input type="password" name="" id="" placeholder="Enter Password" value={pwd} onChange={(event)=>setPwd(event.target.value)} />
+          <br />
+          <br />
+          <input type="text" name="" id="" placeholder="Enter email" value={email} onChange={(event)=>setEmail(event.target.value)} />
+          <br />
+          <br />
+          <button>Submit</button>
+          <button onClick={()=>{setEmail("");
+            setName("");
+            setPwd("");
+          }} >Clear</button>
+          <br />
+          <h3>{name}</h3>
+          <h3>{pwd}</h3>
+          <h3>{email}</h3>
+        </form>
       </div>
     </>
   );
