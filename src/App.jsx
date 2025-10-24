e1e7a01a95207b42f1a0e940f5849059e3694d3b
Apoorv-Saxena08 <coderapoorv08@gmail.com>
2025-10-24 17:37:37 +0530

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,22 @@
-import { use, useState } from "react";
-import Header, { Profile } from "./Header";
-import User from "./User";
-import Wrapper from "./Wrapper";
-import Checkbox from "./Checkbox";
-// function click(){
-//   alert("Bhaar vala");
-// }
+import { use, useEffect, useState } from "react";
 
-function App() {
-  const user = [
-    {
-      id: 1,
-      name: "Apoorv",
-      age: 20,
-    },
-    {
-      id: 2,
-      name: "Anil",
-      age: 25,
-    },
-    {
-      id: 3,
-      name: "Sachin",
-      age: 10,
-    },
-    {
-      id: 4,
-      name: "Yash",
-      age: 29,
-    },
-  ];
+function App(){
+  const [val,setVal] = useState(0);
+
+  function callOnce(){
+    console.log("Hello");
+  }
+
+  useEffect(()=>{
+    callOnce();
+  },[val])
+
 
   return (
     <>
       <div>
-        <h1>Loop in Jsx</h1>
-        <table border="1">
-          <thead>
-            <tr>
-              <td>Id</td>
-              <td>Name</td>
-              <td>Age</td>
-            </tr>
-          </thead>
-
-          <tbody>
-            {user.map((u) => (
-              <tr key={u.id}>
-                <td>{u.id}</td>
-                <td>{u.name}</td>
-                <td>{u.age}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <h1>UseEffect</h1>
+        <button onClick={()=>setVal(val+1)} >Counter {val}</button>
       </div>
     </>
   );
