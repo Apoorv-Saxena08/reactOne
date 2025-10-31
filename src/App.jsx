5821b08cc3bc358bfcc9f6923d459dfb3ca676b4
Apoorv-Saxena08 <coderapoorv08@gmail.com>
2025-10-31 14:24:01 +0530

    Styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
-import { use, useEffect, useState } from "react";
-import Counter from "./Counter";
-import UserProfile from "./UserProfile";
-
-
+import styled from "styled-components";
 function App(){
+
+  //name in capital letter of tag constant
+  const Heading1 = styled.h1`
+  color:red;
+  border:1px solid green
+ 
+  `
 
   return (
   <div >
-
-    <h1 className="heading" >This is the page </h1>
-
-    <div style={{display:"flex" ,flexWrap:"wrap"}} >
-    <UserProfile />
-    <UserProfile />
-    <UserProfile />
-    <UserProfile />
-    <UserProfile />
-    <UserProfile />
-    </div>
-
+      <h1>Styled component</h1>
+      <Heading1>Hello duniya</Heading1>
+      <Heading1>Kseee hooo</Heading1>
   </div>
 
   );
