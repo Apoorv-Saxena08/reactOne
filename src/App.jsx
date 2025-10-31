fca0b424f99e22e403a8e4e0d2f069166fd68f4a
Apoorv-Saxena08 <coderapoorv08@gmail.com>
2025-10-31 14:34:55 +0530

    Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import styled from "styled-components";
+import { Button } from "react-bootstrap";
 function App(){
-
-  //name in capital letter of tag constant
-  const Heading1 = styled.h1`
-  color:red;
-  border:1px solid green
- 
-  `
 
   return (
   <div >
       <h1>Styled component</h1>
-      <Heading1>Hello duniya</Heading1>
-      <Heading1>Kseee hooo</Heading1>
+      <Button variant="info" >Hello</Button>
   </div>
 
   );
