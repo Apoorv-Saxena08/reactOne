1c1c5aa05f24b9a00af0e015db5645f802640a5f
Apoorv-Saxena08 <coderapoorv08@gmail.com>
2025-11-24 09:45:17 +0530

    Updating Objects in State

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
 import { useRef, useState, useTransition } from "react";
-import User from "./User";
-import UserInput from "./UserInput";
-import { useFormStatus } from "react-dom";
-import AddUser from "./AddUser";
-import DisplayUSer from "./DisplayUSer";
-
 
 function App() {
-  const [user, setUser] = useState('');
+  const [data,setData] = useState({
+    name:'',
+    address:{
+      city:'',
+      country:'India'
+    }
+  })
+
+  const handleName=(val)=>{
+    data.name = val;
+    // console.log(val);
+    setData({...data});
+  }
+
+  const handleCity=(val)=>{
+    data.address.city = val;
+    setData({...data,address:{...data.address,val}});
+  }
 
   return (
     <div>
-         <h1>Lifting State up</h1>
+         <h1>Updating Objects in State</h1>
          <br />
+        <input type="text" placeholder="Enter name"
+        onChange={(e)=>handleName(e.target.value)}
+        />
 
-         <AddUser setUser={setUser} />
-         <DisplayUSer user={user}  />
+        <br />
+
+        <input type="text" placeholder="Enter city"
+        onChange={(e)=>handleCity(e.target.value)}
+        />
+        <br /> <br />
+        <h2>{data.name}</h2>
+        <h2>{data.address.city}</h2>
+        <h2>{data.address.country}</h2>
     </div>
   );
 }
